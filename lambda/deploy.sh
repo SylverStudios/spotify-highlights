7210a838fd0a60096097e6a265549f411393d3df
Aaron Votre <avotre422@gmail.com>
2020-01-04 14:59:34 -0500

    ✨Make top artists script work & add deploy script

---
+++ lambda/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pip install requests --target ./package
+
+FILE="spotify_top_artists"
+rm $FILE.zip
+cd package
+zip -r ../$FILE.zip .
+cd ..
+zip -g $FILE.zip top_artists.py
+
+echo "Zip ready:"
+echo $FILE.zip
+
+# aws lambda update-function-code --function-name $NAME --zip-file fileb://$FILE.zip
